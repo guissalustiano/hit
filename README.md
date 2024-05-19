16a715965d8a7bd5fe767f5695b9f93e63a6593f
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-05-19 13:34:15 +0200

    create project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hit - Touch with super powers
+Create file with parents in a single command
+
+WIP: does not does exactly what touch does
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(io_error_more)]
+
+use log::debug;
+use std::{
+    fs::{self, File},
+    io::ErrorKind,
+    path::PathBuf,
+};
+
+use anyhow::Result;
+use clap::Parser;
+
+/// Create a file and parent directory if does not exist.
+#[derive(Parser)]
+struct Cli {
+    /// The path to the file to read
+    path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+    let args = Cli::parse();
+    let path = args.path;
+
+    if path.exists() {
+        debug!("path alredy exist");
+        return Ok(());
+    }
+
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(&parent)?;
+    }
+
+    debug!("Try to create a path");
+    match File::create_new(&path) {
+        Err(err) if err.kind() == ErrorKind::IsADirectory => {
+            debug!("It was a dir, try to create a dir");
+            fs::create_dir(&path)?;
+        }
+        others => {
+            others?;
+        }
+    };
+
+    Ok(())
 }
